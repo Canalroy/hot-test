b608bd0458112eb92733469c06a2a03f78179158
Canalroy <ryota.kanamori@ctc-g.co.jp>
2017-03-17 16:00:09 +0900

    Update get-information.ps1

--- get-information.ps1
+++ get-information.ps1
@@ -1,26 +1,25 @@
-'#######Windows 情報取得######'
-'===ベンダー名==='
+'#######Windows Server Informations######'
+'===Vendor Name==='
 wmic csproduct get vendor
-'===シリアルナンバー==='
+'===Serial Number==='
 wmic bios get serialnumber
-'===ホスト名==='
+'===Hostname==='
 hostname
-'===IP==='
+'===IP address==='
 ipconfig /all
-'===CPU型番 / CPU数 / コア数 / CPU==='
+'===CPU Model / Number of CPUs / Number of Core/ CPU Clock Speed ==='
 Get-WmiObject -Class Win32_Processor | FL Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed
-'===メモリ量==='
+'===Memory amount==='
 ((Get-WmiObject -Class Win32_physicalMemory).capacity /1024/1024/1024).toString() + " GB"
-'===HD容量==='
+'===Disk Info==='
 Get-WmiObject Win32_LogicalDisk | format-list DeviceID, Name, VolumeName, DeviceType, Filesystem, ProviderName, Size, FreeSpace
-'===パーティション構成==='
+'===Partition Info==='
 Get-WmiObject Win32_LogicalDisk | format-list DeviceID, Name, VolumeName, DeviceType, Filesystem, ProviderName, Size, FreeSpace
-'===RAID構成==='
-'===OS名 / OS Version==='
+'===OS name / OS Version==='
 Get-WmiObject -Class Win32_OperatingSystem | Format-List Caption, csdversion, OSArchitecture, BuildNumber, countrycode, Locale, MUILanguages, Organization
 
-'===導入SW==='
+'===SW Information==='
 get-windowsfeature
 get-wmiobject -class win32_product | format-list Name, Vendor, Version
-'===ライセンス情報==='
+'===License Information==='
 cscript.exe C:\windows\System32\slmgr.vbs /dlv
